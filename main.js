025e1e4432e0c0b883f99bd33a1f83f693e4ef6b
crocJohnDee <johnn.barendrecht@hotmail.com>
2019-08-29 16:24:24 +0200

    minor change

--- main.js
+++ main.js
@@ -2,11 +2,11 @@ const form = document.querySelector('form');
 form.addEventListener('submit', (e) => {
     e.preventDefault();
 
-    let crypto = (e.target[0].value).toLowerCase();
+    let crypto = (e.target[0].value);
     let amount = e.target[1].value;
-    let fiat = (e.target[2].value).toLowerCase();
+    let fiat = (e.target[2].value);
 
-    fetch(`https://api.cryptonator.com/api/ticker/${crypto}-${fiat}`)
+    fetch(`https://api.cryptonator.com/api/ticker/${(crypto.toLowerCase())}-${(fiat).toLowerCase()}`)
         .then((response) => response.json())
         .then((myJson) => {
             let data = JSON.parse(JSON.stringify(myJson));
