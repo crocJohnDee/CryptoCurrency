a5690c506026b7bb679de4e221f5a59814e1639a
crocJohnDee <johnn.barendrecht@hotmail.com>
2019-08-30 09:42:04 +0200

    added some cryptos

--- main.js
+++ main.js
@@ -1,3 +1,115 @@
+const cryptos = [
+    "ETH",
+    "XRP",
+    "BCH",
+    "USDT",
+    "LTC",
+    "BNB",
+    "EOS",
+    "BSV",
+    "XLM",
+    "ADA",
+    "XMR",
+    "LEO",
+    "TRX",
+    "HT",
+    "DASH",
+    "ETC",
+    "MIOTA",
+    "XTZ",
+    "LINK",
+    "NEO",
+    "MKR",
+    "USDC",
+    "XEM",
+    "ATOM",
+    "ONT",
+    "CRO",
+    "ZEC",
+    "DOGE",
+    "VSYS",
+    "HEDG",
+    "DCR",
+    "VET",
+    "BAT",
+    "QTUM",
+    "PAX",
+    "TUSD",
+    "BTG",
+    "OMG",
+    "ZB",
+    "RVN",
+    "LSK",
+    "NANO",
+    "KCS",
+    "INB",
+    "BCD",
+    "WAVES",
+    "BTT",
+    "ALGO",
+    "NRG",
+    "HOT",
+    "ICX",
+    "LAMB",
+    "BCN",
+    "THETA",
+    "EGT",
+    "DGB",
+    "HC",
+    "ZRX",
+    "BTS",
+    "NPXS",
+    "IOST",
+    "REP",
+    "NEX",
+    "MONA",
+    "BTM",
+    "KMD",
+    "DAI",
+    "MAID",
+    "SC",
+    "QNT",
+    "AOA",
+    "XVG",
+    "RIF",
+    "WIN",
+    "ZIL",
+    "AE",
+    "ETP",
+    "GNT",
+    "STEEM",
+    "ABBC",
+    "SNT",
+    "ARDR",
+    "MCO",
+    "ENJ",
+    "REN",
+    "WTC",
+    "GXC",
+    "SNX",
+    "XZC",
+    "SOLVE",
+    "WAX",
+    "BEAM",
+    "STRAT",
+    "ELA",
+    "NEXO",
+    "ELF",
+    "GRIN",
+    "PAI",
+    "EURS",
+    "ZEN"
+]
+
+let cryptoOption = document.querySelector("#crypto-currency");
+for (let i = 0; i < cryptos.length; i++) {
+    let option = document.createElement("option");
+    let text = document.createTextNode(cryptos[i])
+    option.appendChild(text);
+    cryptoOption.appendChild(option);
+
+}
+
 const form = document.querySelector('form');
 form.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -11,7 +123,7 @@ form.addEventListener('submit', (e) => {
         .then((myJson) => {
             let data = JSON.parse(JSON.stringify(myJson));
             document.querySelector("#output")
-                .innerHTML = `${fiat} ${(data.ticker.price * amount).toFixed(2)}`
+                .innerHTML = `${fiat} ${(data.ticker.price * amount).toLocaleString()}`
         });
 });
 
